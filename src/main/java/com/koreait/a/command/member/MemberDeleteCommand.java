2404b73dd1e8972c0998e30438bd585ce6525180
pokrogo2 <pokrogo@gmail.com>
2021-07-23 10:59:49 +0900

    modifyMember

--- src/main/java/com/koreait/a/command/member/MemberDeleteCommand.java
+++ src/main/java/com/koreait/a/command/member/MemberDeleteCommand.java
@@ -1,7 +1,9 @@
 package com.koreait.a.command.member;
 
+import java.io.IOException;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.apache.ibatis.session.SqlSession;
@@ -17,15 +19,29 @@ public class MemberDeleteCommand implements MemberCommand {
 		// TODO Auto-generated method stub
 		Map<String, Object> map = model.asMap();
 		HttpSession session = (HttpSession)map.get("session");
+		HttpServletResponse response = (HttpServletResponse)map.get("response");
 		
 		long no = ((MemberDTO)session.getAttribute("loginUser")).getMemberNo();
 		System.out.println(no);
 		MemberDAO memberDAO = sqlSession.getMapper(MemberDAO.class);
 		int count = memberDAO.delete(no);
-		
+		response.setContentType("html/text; charset=UTF-8");
+		try {
 		if (count > 0) {
 			session.invalidate();
+			response.getWriter().println("<script>");
+			response.getWriter().println("alert('탈퇴에 성공하였습니다.')");
+			response.getWriter().println("location.href='index.do'");
+			response.getWriter().println("</script>");
+		}else {
+			response.getWriter().println("<script>");
+			response.getWriter().println("alert('탈퇴에 실패하였습니다.')");
+			response.getWriter().println("location.href='index.do'");
+			response.getWriter().println("</script>");
 		}
+	}catch(IOException e) {
+		e.printStackTrace();
+	}
 	}
 
 }
